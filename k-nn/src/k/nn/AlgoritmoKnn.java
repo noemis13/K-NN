e4d1a590c2fb2596ef5abb507b3bd63fc6904ea9
noemis13 <noemischerer13@gmail.com>
2016-05-23 12:07:59 -0300

    ah, falta ainda

--- k-nn/src/k/nn/AlgoritmoKnn.java
+++ k-nn/src/k/nn/AlgoritmoKnn.java
@@ -8,11 +8,6 @@ import java.util.ArrayList;
 import java.util.LinkedList;
 import java.util.List;
 
-/**
- *
- * @author noemi
- */
-
 /* 1: Abre os dois arquivos: teste e treino
  2: Normaliza os dados do arquivo usando max-min ou z-score
  3: Aplicar k-nn (mahathan ou euclidiana)
@@ -20,48 +15,15 @@ import java.util.List;
  */
 public class AlgoritmoKnn {
 
-    /*public String lerDadosTeste() throws FileNotFoundException, IOException {
-        FileReader dadosTeste = new FileReader("teste.data");
-        BufferedReader lerArquivoTeste = new BufferedReader(dadosTeste);
-
-        String linhaTeste = lerArquivoTeste.readLine();
-        while (linhaTeste != null) {
-            //System.out.println(linhaTeste);
-            linhaTeste = lerArquivoTeste.readLine();
-            return linhaTeste;
-        
-        }
-        return null;
-        
-        
-    }
-
-    public void lerDadosTreino() throws FileNotFoundException, IOException {
-        FileReader dadosTreino = new FileReader("treino.data");
-        BufferedReader lerArquivoTreino = new BufferedReader(dadosTreino);
-
-        String linhaTreino = lerArquivoTreino.readLine();
-        while (linhaTreino != null) {
-            System.out.println(linhaTreino);
-            linhaTreino = lerArquivoTreino.readLine();
-            //return linhaTreino;
-        }
-        //return null;
-    }*/
-
     public void normalizarDados() throws IOException {
         //---------------LER ARQUIVOS------------//
         FileReader dadosTeste = new FileReader("teste.data");
         BufferedReader lerArquivoTeste = new BufferedReader(dadosTeste);
+        String linhaTeste = lerArquivoTeste.readLine();
+        
         FileReader dadosTreino = new FileReader("treino.data");
         BufferedReader lerArquivoTreino = new BufferedReader(dadosTreino);
-
-        
-        String linhaTeste = lerArquivoTeste.readLine();
         String linhaTreino = lerArquivoTreino.readLine();
-        
-        String tiraMeses;
-        
         
         /* Usando o z-score
         1: aplica a media de todos os elementos
@@ -69,57 +31,213 @@ public class AlgoritmoKnn {
         3: normaliza os dados usando: v0i = (vi − A¯)/σA
         */
         
-        int mediaTeste = 0, desvioPadraoTeste = 0;
-        int contValores = 0;
+        double mediaTeste = 0, desvioPadraoTeste = 0;
+        int tamTeste = 0;
+        String apenasNumeros = new String();
+        ArrayList<String> valoresTeste = new ArrayList<>();
         
         while (linhaTeste != null) {
-            //colocar condicao
-            contValores++;
-            mediaTeste = mediaTeste + Integer.parseInt(linhaTeste);
+            if(linhaTeste.contains("janeiro")) {
+               apenasNumeros  = linhaTeste.replace("janeiro", "");
+               valoresTeste.add(apenasNumeros);
+                       
+            }else if(linhaTeste.contains("fevereiro")){
+               apenasNumeros  = linhaTeste.replace("fevereiro", "");
+               valoresTeste.add(apenasNumeros);
+               
+            }else if(linhaTeste.contains("maio")){
+               apenasNumeros  = linhaTeste.replace("maio", "");
+               valoresTeste.add(apenasNumeros);
+               
+            }else if(linhaTeste.contains("abril")){
+               apenasNumeros  = linhaTeste.replace("abril", "");
+               valoresTeste.add(apenasNumeros);
+               
+            }else if(linhaTeste.contains("junho")){
+               apenasNumeros  = linhaTeste.replace("junho", "");
+               valoresTeste.add(apenasNumeros);
+               
+            }else if(linhaTeste.contains("julho")){
+               apenasNumeros  = linhaTeste.replace("julho", "");
+               valoresTeste.add(apenasNumeros);
+               
+            }else if(linhaTeste.contains("agosto")){
+               apenasNumeros  = linhaTeste.replace("agosto", "");
+               valoresTeste.add(apenasNumeros);
+               
+            }else if(linhaTeste.contains("setembro")){
+               apenasNumeros  = linhaTeste.replace("setembro", "");
+               valoresTeste.add(apenasNumeros);
+                       
+            }else if(linhaTeste.contains("outubro")){
+               apenasNumeros  = linhaTeste.replace("outubro", "");
+               valoresTeste.add(apenasNumeros);
+                        
+            }else if(linhaTeste.contains("novembro")){
+               apenasNumeros  = linhaTeste.replace("novembro", "");
+               valoresTeste.add(apenasNumeros);
+               
+            }else if(linhaTeste.contains("desembro")){
+               apenasNumeros  = linhaTeste.replace("dezembro", "");
+               valoresTeste.add(apenasNumeros);
+            }
             linhaTeste = lerArquivoTeste.readLine();
-            
         }
-        mediaTeste = mediaTeste/contValores;
-        //calcular desvio padrao
         
-        /*
-        Calcular para cada valor e salvar em uma lista
+        tamTeste = valoresTeste.size();
+        double sum = 0; 
+        int count = 0;
+        //--ACHA MEDIA
+        for (int i = 0; i < tamTeste; i++) {
+            int tam = valoresTeste.get(i).trim().split(",").length;
+            count += tam;            
+            for(int k = 0; k < tam; k++){
+                sum += Double.valueOf(valoresTeste.get(i).split(",")[k]);
+            }
+           
+        }
+         mediaTeste = sum / count;
+         
+         //--ACHA DESVIO
+         double desvio=0, desvioQuadrado = 0, desvioSub=0, mediaQuadrado;
+         int contDesvioPadrao = 0;
+         
+         for (int i = 0; i < tamTeste; i++) {
+             int valor = valoresTeste.get(i).trim().split(",").length;
+             contDesvioPadrao += valor;
+             for (int j = 0; j < valor; j++) {
+                desvio = Double.valueOf(valoresTeste.get(i).split(",")[j]);
+                desvioSub = desvio - mediaTeste;
+                desvioQuadrado = desvioQuadrado + (desvioSub*desvioSub);
+                
+             }
+         }
+         mediaQuadrado = desvioQuadrado/contDesvioPadrao;
+         desvioPadraoTeste = Math.sqrt(mediaQuadrado);
+        
+         //--NORMALIZA
+        
+        double normalizaDadosTeste, valor = 0, calcNormaliza;
+        ArrayList<Double> dadosNormalizadosTeste = new ArrayList<>();
+        for (int i = 0; i < tamTeste; i++) {
+            int dados = valoresTeste.get(i).trim().split(",").length;
+            for (int j = 0; j < dados; j++) {
+                 valor = Double.valueOf(valoresTeste.get(i).split(",")[j]);
+                 calcNormaliza = (valor-mediaTeste)/desvioPadraoTeste;
+                 dadosNormalizadosTeste.add(calcNormaliza); 
+            }
+        }
+    
+        
+        /* Usando o z-score
+        1: aplica a media de todos os elementos
+        2: aplica o desvio padrão
+        3: normaliza os dados usando: v0i = (vi − A¯)/σA
         */
-        int normalizaDadosTeste;
-        ArrayList<Integer> dadosNormalizadosTeste = new ArrayList<>();
-        while(linhaTeste != null) {
-            normalizaDadosTeste = (Integer.parseInt(linhaTeste)-mediaTeste)/desvioPadraoTeste;
-            dadosNormalizadosTeste.add(normalizaDadosTeste);
+        
+        double mediaTreino = 0, desvioPadraoTreino = 0;
+        
+        int tamTreino = 0;
+        String apenasNumerosTreino = new String();
+        ArrayList<String> valoresTreino = new ArrayList<>();
+        
+        while (linhaTreino != null) {
+            if(linhaTreino.contains("janeiro")) {
+               apenasNumerosTreino  = linhaTreino.replace("janeiro", "");
+               valoresTreino.add(apenasNumeros);
+                       
+            }else if(linhaTreino.contains("fevereiro")){
+               apenasNumerosTreino  = linhaTreino.replace("fevereiro", "");
+               valoresTreino.add(apenasNumeros);
+               
+            }else if(linhaTreino.contains("maio")){
+               apenasNumerosTreino = linhaTreino.replace("maio", "");
+               valoresTreino.add(apenasNumeros);
+               
+            }else if(linhaTreino.contains("abril")){
+               apenasNumerosTreino = linhaTreino.replace("abril", "");
+               valoresTreino.add(apenasNumeros);
+               
+            }else if(linhaTreino.contains("junho")){
+               apenasNumerosTreino = linhaTreino.replace("junho", "");
+               valoresTreino.add(apenasNumeros);
+               
+            }else if(linhaTreino.contains("julho")){
+               apenasNumerosTreino = linhaTreino.replace("julho", "");
+               valoresTreino.add(apenasNumeros);
+               
+            }else if(linhaTreino.contains("agosto")){
+               apenasNumerosTreino = linhaTreino.replace("agosto", "");
+               valoresTreino.add(apenasNumeros);
+               
+            }else if(linhaTreino.contains("setembro")){
+               apenasNumerosTreino = linhaTreino.replace("setembro", "");
+               valoresTreino.add(apenasNumeros);
+                       
+            }else if(linhaTreino.contains("outubro")){
+               apenasNumerosTreino = linhaTreino.replace("outubro", "");
+               valoresTreino.add(apenasNumeros);
+                        
+            }else if(linhaTreino.contains("novembro")){
+               apenasNumerosTreino = linhaTreino.replace("novembro", "");
+               valoresTreino.add(apenasNumeros);
+               
+            }else if(linhaTreino.contains("desembro")){
+               apenasNumerosTreino = linhaTreino.replace("dezembro", "");
+               valoresTreino.add(apenasNumeros);
+            }
+            linhaTreino = lerArquivoTreino.readLine();
         }
         
-        /**
-         * NORMALIZAR DADOS TREINO
-         */
-  
-        int mediaTreino = 0, desvioPadraoTreino = 0;
-        int contValoresTreino = 0;
-        
-        while (linhaTeste != null) {
-            //colocar condicao
-            contValores++;
-            mediaTreino = mediaTreino + Integer.parseInt(linhaTeste);
-            linhaTreino = lerArquivoTeste.readLine();
-            
+        tamTreino = valoresTreino.size();
+        double sumTreino = 0; 
+        int countTreino = 0;
+        //--ACHA MEDIA
+        for (int i = 0; i < tamTreino; i++) {
+            int tam = valoresTreino.get(i).trim().split(",").length;
+            countTreino += tam;            
+            for(int k = 0; k < tam; k++){
+                sumTreino += Double.valueOf(valoresTreino.get(i).split(",")[k]);
+            }
+           
         }
-        mediaTeste = mediaTeste/contValores;
-        //calcular desvio padrao
+         mediaTreino = sumTreino / countTreino;
+         
+         //--ACHA DESVIO
+         double desvioTreino=0, desvioQuadradoTreino = 0, desvioSubTreino=0, mediaQuadradoTreino;
+         int contDesvioPadraoTreino = 0;
+         
+         for (int i = 0; i < tamTreino; i++) {
+             int valorTreino = valoresTreino.get(i).trim().split(",").length;
+             contDesvioPadraoTreino += valorTreino;
+             for (int j = 0; j < valorTreino; j++) {
+                desvioTreino = Double.valueOf(valoresTreino.get(i).split(",")[j]);
+                desvioSubTreino = desvioTreino - mediaTreino;
+                desvioQuadradoTreino = desvioQuadradoTreino + (desvioSubTreino*desvioSubTreino);
+                
+             }
+         }
+         mediaQuadradoTreino = desvioQuadradoTreino/contDesvioPadraoTreino;
+         desvioPadraoTreino = Math.sqrt(mediaQuadradoTreino);
         
-        int normalizaDadosTreino;
-        ArrayList<Integer> dadosNormalizadosTreino = new ArrayList<>();
-        while(linhaTeste != null) {
-            normalizaDadosTreino = (Integer.parseInt(linhaTreino)-mediaTreino)/desvioPadraoTreino;
-            dadosNormalizadosTreino.add(normalizaDadosTreino);
+         //--NORMALIZA
+        
+        double valorTreino = 0, calcNormalizaTreino;
+        ArrayList<Double> dadosNormalizadosTreino = new ArrayList<>();
+        for (int i = 0; i < tamTreino; i++) {
+            int dados = valoresTreino.get(i).trim().split(",").length;
+            for (int j = 0; j < dados; j++) {
+                 valorTreino = Double.valueOf(valoresTreino.get(i).split(",")[j]);
+                 calcNormalizaTreino = (valorTreino-mediaTreino)/desvioPadraoTreino;
+                 dadosNormalizadosTreino.add(calcNormalizaTreino);
+                 
+            }
         }
-        
+        System.out.println(dadosNormalizadosTreino);
         
     }
     
-    public void aplicaDistanciaManhattan(int valorK, ArrayList<Integer> dadosNormalizadosTreino, ArrayList<Integer> dadosNormalizadosTeste) {
+    public void aplicaDistanciaManhattan(int valorK) {
         
     }
 
